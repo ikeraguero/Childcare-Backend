6cd3662214dc31d7eb10b9923c7efda5fd51d244
ikeraguero <ikerpires407@gmail.com>
2022-09-23 09:46:04 -0300

    first commit

---
+++ src/controllers/apicontroller.ts
@@ -0,0 +1,83 @@
+import { Request, Response } from 'express';
+import { getChildren, getChild, getSchools, getSchool, getDonations, getDonation } from "../../db/search";
+import { addChild, deleteChild, updateChild, addSchool, deleteSchool, updateSchool, addDonation } from "../../db/post";
+
+// Children
+
+export const children = async (req: Request, res: Response) => {
+    const children = await getChildren();
+    res.json(children.content);
+}
+
+export const child = async (req: Request, res: Response) => {
+    const child = await getChild(req.params.child_id);
+    res.json(child.content);
+}
+
+export const childadd = async (req: Request, res: Response) => {
+    const r = req.body;
+    const child = await addChild(r.childphoto, r.childname, r.childgender,r.childschool, r.childage, r.childaddress, r.childcity, r.childstate, r.childcpf, r.is_castrated);
+    res.redirect("http://localhost:8080/admin");
+
+}
+
+
+export const childdelete = async (req: Request, res: Response) => {
+    const child = await deleteChild(req.params.child_id);
+    res.json(child);
+}
+
+export const childupdate = async (req: Request, res: Response) => {
+    const r = req.body;
+    const child = await updateChild(req.params.child_id, r.childphoto, r.childname, r.childgender,r.childschool, r.childage, r.childaddress, r.childcity, r.childstate, r.childcpf, r.is_castrated);
+    res.redirect("http://localhost:8080/admin");
+}
+
+// Schools
+
+export const schools = async (req: Request, res: Response) => {
+    const schools = await getSchools();
+    res.json(schools.content);
+}
+
+export const school = async (req: Request, res: Response) => {
+    const school = await getSchool(req.params.school_id);
+    res.json(school.content);
+}
+
+export const schooladd = async (req: Request, res: Response) => {
+    const r = req.body;
+    const school = await addSchool(r.schoolemail, r.schoolcnpj, r.schoolcellphone, r.schoolphoto, r.schoolname, r.schooladdress, r.schoolcity, r.schoolstate);
+    res.redirect("http://localhost:8080/admin");
+
+}
+
+export const schooldelete = async (req: Request, res: Response) => {
+    const school = await deleteSchool(req.params.school_id);
+    res.json(school);
+}
+
+export const schoolupdate = async (req: Request, res: Response) => {
+    const r = req.body;
+    const school = await updateSchool(req.params.school_id, r.schoolemail, r.schoolcnpj, r.schoolcellphone, r.schoolphoto, r.schoolname, r.schooladdress, r.schoolcity, r.schoolstate);
+    res.redirect("http://localhost:8080/admin");
+}
+
+// Donations
+
+export const donations = async (req: Request, res: Response) => {
+    const donations = await getDonations();
+    res.json(donations.content);
+}
+
+export const donation = async (req: Request, res: Response) => {
+    const donation = await getDonation(req.params.donation_id);
+    res.json(donation.content);
+}
+
+export const donationadd = async (req: Request, res: Response) => {
+    const r = req.body;
+    const donation = await addDonation(r.donationdonatortype, r.donationdonator, r.donationemail, r.donationcpf, r.donationcnpj, r.donationcellphone, r.donationtype, r.donationvalue, r.donationmaterials);
+    res.redirect("http://localhost:8080/admin");
+
+}
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { auth } from 'express-openid-connect';
+import { config } from "../auth/authConfig";
+import * as api from "./controllers/apicontroller";
+
+const app = express();
+dotenv.config();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(auth(config));
+
+const port = process.env.PORT || 3000;
+
+
+
+// GET
+app.get("/api/children", api.children);
+app.get("/api/child/:child_id", api.child);
+
+app.get("/api/schools", api.schools);
+app.get("/api/school/:school_id", api.school);
+
+app.get("/api/donations", api.donations);
+app.get("/api/donation/:donation_id", api.donation);
+
+
+
+// POST
+app.post("/api/schooladd", api.childadd);
+app.post("/api/schoolupdate/:school_id", api.childupdate);
+app.post("/api/schooldel/:school_id", api.childdelete);
+
+app.post("/api/childadd", api.childadd);
+app.post("/api/childupdate/:child_id", api.childupdate);
+app.post("/api/childdel/:child_id", api.childdelete);
+
+app.post("/api/donationadd", api.donationadd);
+
+
+
+//
+
+app.listen(port, () => {
+    console.log( `Port: http://localhost:${ port }.` );
+});
+
