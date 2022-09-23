e8b2f7a60ecf8f649b15ed4fc5900995072daa3b
ikeraguero <ikerpires407@gmail.com>
2022-09-23 10:17:03 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ app.post("/api/donationadd", api.donationadd);
 
 //
 
-app.listen(port, () => {
-    console.log( `Port: http://localhost:${ port }.` );
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
